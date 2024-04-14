668ea7102bd0609f6c3e126e60dfca144341239f
Guilherme Coelho <vestgui@gmail.com>
2024-04-14 16:13:39 -0300

    Menu inferior, foi tirada a sombra e arrumado o espaçamento.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,4 +41,9 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //Material Design
+    implementation("androidx.compose.material:material:1.6.5")
+    implementation("androidx.compose.material3:material3-window-size-class:1.2.1")
+    implementation("androidx.compose.material3:material3-adaptive-navigation-suite:1.0.0-alpha05")
 }
